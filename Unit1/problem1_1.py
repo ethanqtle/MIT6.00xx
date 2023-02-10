b6a3aebf2080b1445544ecbf76131cfca29ec199
Nguyen Le <lenguyen_d@hotmail.com>
2023-02-10 08:15:49 -0800

    Update comments

--- Unit1/problem1_1.py
+++ Unit1/problem1_1.py
@@ -7,7 +7,9 @@ Created on Tue Feb  7 13:56:39 2023
 s = 'azcbobobegghakl'
 # Assume s is a string of lower case characters.
 
-# Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
+# Write a program that counts up the number of vowels contained in the string s.
+# Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, 
+# if s = 'azcbobobegghakl', your program should print:
 
 # Number of vowels: 5
 
