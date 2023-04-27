d0342f19803bb17ef646cb4e78035ed4791346be
Nguyen Le <lenguyen_d@hotmail.com>
2023-04-26 21:11:29 -0700

    Add testcase from Edx website

--- 6.002x/Unit3/ProblemSet3/ps3b.py
+++ 6.002x/Unit3/ProblemSet3/ps3b.py
@@ -509,36 +509,6 @@ def simulationWithDrug(numViruses, maxPop, maxBirthProb, clearProb, resistances,
     pylab.show()
 
 
-# In this problem, we will use the implementation you filled in 
-# for Problem 4 to run a simulation. You will create a TreatedPatient instance 
-# with the following parameters, then run the simulation:
-# viruses, a list of 100 ResistantVirus instances
-
-# maxPop, maximum sustainable virus population = 1000
-
-# Each ResistantVirus instance in the viruses list should be initialized 
-# with the following parameters:
-
-# maxBirthProb, maximum reproduction probability for a virus particle = 0.1
-
-# clearProb, maximum clearance probability for a virus particle = 0.05
-
-# resistances, The virus's genetic resistance to drugs in the experiment = {'guttagonol': False}
-
-# mutProb, probability of a mutation in a virus particle's offspring = 0.005
-
-# Run a simulation that consists of 150 time steps, followed by the addition of the 
-# drug, guttagonol, followed by another 150 time steps. You should make use of the
-# function simulationWithDrug(numViruses, maxPop, maxBirthProb, clearProb, resistances, mutProb, numTrials).
-# 
-# As with problem 3, perform up to 100 trials and make sure that your results are repeatable and representative.
-
-# Create one plot that records both the average total virus population and the average population of guttagonol-resistant virus particles over time.
-
-# A few good questions to consider as you look at your plots are: What trends do you observe? Are the 
-# trends consistent with your intuition? Feel free to discuss the answers to these questions in the 
-# forum, to fully cement your understanding of this problem set, processing and interpreting data.
-
-# Call simulationWithDrug with the following parameters:
-numTrials = 100
-simulationWithDrug(100, 1000, 0.1, 0.05, {'guttagonol': False}, 0.005, numTrials)
+simulationWithDrug(1, 10, 1.0, 0.0, {}, 1.0, 5)
+simulationWithDrug(1, 20, 1.0, 0.0, {"guttagonol": True}, 1.0, 5)
+simulationWithDrug(75, 100, .8, 0.1, {"guttagonol": True}, 0.8, 1)
